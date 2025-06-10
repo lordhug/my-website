6ac46cf8c8577a16d8be098583403a78831b806b
lordhug <Lordhug338@gmail.com>
2025-06-10 08:35:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function sayHello() {
-  alert("Hello, I've made this with javascript for my homework!");
-}
+    let username = document.getElementById("usernameInput").value;
+    document.getElementById("greeting").innerText = `Hello ${username}`;
+    alert(`hi ${username}, nice to meet you`);
+  }
